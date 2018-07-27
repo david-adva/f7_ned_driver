5006f03d66406dd9dcad55ccdbbe21426322e4a1
user <user@vm.advaoptical.com>
2018-07-27 19:01:28 +0000

    update

--- demo_webdriver_imp_wait.py
+++ demo_webdriver_imp_wait.py
@@ -15,12 +15,22 @@ from selenium import webdriver
 
 
 def get_wait_time(driver, wait_value, ele):
-    if wait_value:
-        driver.implicitly_wait(wait_value)
-
-    print('Start to tick...')
+    print('Using implicitly_wait(). Start to tick...')
     start = time.time()
     try:
+        driver.implicitly_wait(wait_value)
+        driver.find_element_by_id(ele)
+    except Exception:
+        pass
+    end = time.time()
+    print('Time passed are %i seconds!\n' % int(end - start))
+
+
+def true_wait(driver, wait_value, ele):
+    print('Using time.sleep(). Start to tick...')
+    start = time.time()
+    try:
+        time.sleep(wait_value)
         driver.find_element_by_id(ele)
     except Exception:
         pass
@@ -34,12 +44,18 @@ def main():
         my_web.get("https://www.google.com")
 
         while True:
+            mode = input(
+                "Pick the waiting mode, 'S' for sleep, " +
+                "'I' for implicitly_wait: ")
             my_wait = input(
                 "Input the wait time in seconds (ctrl-c to exit): ")
             my_id = input(
                 "Input the ID of the element to search, e.g. 'main': ")
 
-            get_wait_time(my_web, my_wait, my_id)
+            if mode == 'S':
+                true_wait(my_web, my_wait, my_id)
+            elif mode == 'I':
+                get_wait_time(my_web, my_wait, my_id)
 
     except KeyboardInterrupt:
         print('\nExit on KeyboardInterrupt!')
